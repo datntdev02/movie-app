1cc5e3277153c7291926a8e68ab698f087541f2b
datntdev02 <datntdev02@gmail.com>
2023-06-11 21:28:29 +0700

    update-slider

--- src/components/slider-image/SliderImage.js
+++ src/components/slider-image/SliderImage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import './SliderImage.css'
+import './SliderImage.scss'
 import { Carousel } from 'react-responsive-carousel';
 import { Trending } from '../../shared/ListOfTrending';
 
@@ -9,11 +9,11 @@ function SliderImage() {
         <div className='slider-container'>
             <h2>Trending Now</h2>
             <div className='slider-show'>
-                <Carousel className='slider'>
+                <Carousel autoPlay infiniteLoop>
                     {Trending.map((trending) => (
                         <div key={trending.id}>
-                            <img src={trending.img} alt=''/>
-                            <p>{trending.title}</p>
+                            <img src={trending.img} alt='' />
+                            <div className='title'>{trending.title}</div>
                         </div>
                     ))}
                 </Carousel>
